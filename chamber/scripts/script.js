259fe51424f324e145290f3df96901ea8ff0fd69
adiquatech <quadrikolawoleadisa@gmail.com>
2024-09-28 08:25:34 +0100

    new

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -12,114 +12,12 @@ document.addEventListener("DOMContentLoaded", () => {
 document.getElementById('hamburger').addEventListener('click', function() {
     const nav = document.getElementById('navigation');
     const hamburger = document.getElementById('hamburger');
-    
-    // Toggle the 'active' class for the nav and hamburger menu
     nav.classList.toggle('active');
     
     // Change the icon between hamburger and X
     if (hamburger.textContent === '☰') {
-        hamburger.textContent = '✖'; // Change to 'X' when active
+        hamburger.textContent = '✖';
     } else {
-        hamburger.textContent = '☰'; // Change back to hamburger icon
+        hamburger.textContent = '☰';
     }
 });
-
-
-// Chamber members
-const url = "https://adiquatech.github.io/wdd231/chamber/data/members.json";
-const cards = document.getElementById("cards");
-const contributorsCard = document.querySelector("#contributors");
-
-async function getMemberData() {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    displayMembers(data.members);
-    displayGoldMembers(data.members);
-}
-
-getMemberData();
-
-const displayMembers = (members) => {
-    members.forEach(m => {
-        const card = document.createElement("section");
-        const name = document.createElement("h3");
-        const image = document.createElement("img");
-        const info = document.createElement("div");
-        
-        
-        info.textContent = `📞${m.phone} ✉️${m.email} | 📍${m.address}`
-        name.textContent = m.name
-
-        image.setAttribute("src", m.img);
-        image.setAttribute("alt", `The brand icon of ${m.name}`);
-        image.setAttribute("loading", "lazy");
-        image.setAttribute("width", "200");
-        image.setAttribute("height", "auto");
-
-        card.appendChild(name);
-        card.appendChild(image);
-        card.appendChild(info);
-        cards?.appendChild(card);
-    });
-}
-
-
-const gridButton = document.querySelector("#grid");
-const listButton = document.querySelector("#list");
-
-listButton?.addEventListener("click", () => {
-    cards.classList.add("list");
-});
-
-gridButton?.addEventListener("click", () => {
-    cards.classList.remove("list");
-});
-
-//========== DISPLAY GOLD MEMBERS ==========
-
-// function displayGoldMembers(members) {
-//     const goldMembers = shuffle(members)
-
-//     goldMembers.forEach(m => {
-//         if (m.memberlevel === "Gold") {
-//             const card = document.createElement("section");
-//             const name = document.createElement("h4");
-//             const image = document.createElement("img");
-//             const info = document.createElement("div");
-            
-            
-//             info.textContent = `📞${m.phone} ✉️${m.email} | 📍${m.address}`
-//             name.textContent = m.name
-
-//             image.setAttribute("src", m.img);
-//             image.setAttribute("alt", `The brand icon of ${m.name}`);
-//             image.setAttribute("loading", "lazy");
-//             image.setAttribute("width", "100");
-//             image.setAttribute("height", "auto");
-
-//             card.appendChild(name);
-//             card.appendChild(image);
-//             card.appendChild(info);
-//             contributorsCard?.appendChild(card);
-//         } 
-//     });
-// }
-
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
-    while (currentIndex > 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
-  }
