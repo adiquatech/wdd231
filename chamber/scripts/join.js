5f221e27e636afcafcdac529136e49687fa0afe1
adiquatech <quadrikolawoleadisa@gmail.com>
2024-10-13 10:24:39 +0100

    new

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -5,6 +5,7 @@ const mytitle = document.querySelector("#dialog-title");
 const myinfo = document.querySelector("#dialog-content");
 const myclose = document.querySelector("#close-dialog");
 
+
 myclose.addEventListener("click", () => mydialog.close());
 
 // Fetch JSON data
@@ -37,3 +38,25 @@ function displayMemberLevels(memberLevels) {
     memberLevelsCard.appendChild(card);
   });
 }
+
+function getDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+  var hh = today.getHours();
+  var min = today.getMinutes();
+  var ss = today.getSeconds();
+
+  // Format the date and time properly
+  if (dd < 10) { dd = '0' + dd; }
+  if (mm < 10) { mm = '0' + mm; }
+  if (hh < 10) { hh = '0' + hh; }
+
+  var formattedDate = yyyy + "-" + mm + "-" + dd;
+  var timestamp = formattedDate;
+
+  document.getElementById("timestamp").value = timestamp;
+  console.log(timestamp);
+}
+getDate();
