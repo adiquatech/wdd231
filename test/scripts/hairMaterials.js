5513057e098583af83d24bfa7aee7027cf9a1337
Adiquatech <quadrikolawoleadisa@gmail.com>
2024-10-28 17:35:15 +0100

    new

--- test/scripts/hairMaterials.js
+++ test/scripts/hairMaterials.js
@@ -8,12 +8,13 @@ async function fetchHairMaterials() {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        const hairMaterials = await response.json();
-        displayHairMaterials(hairMaterials);
+        const data = await response.json();
+        displayHairMaterials(data.hairMaterials); // Access the array with `data.hairMaterials`
     } catch (error) {
         console.error('Error fetching hair materials:', error);
     }
 }
+
 
 // Function to display hair materials
 function displayHairMaterials(hairMaterials) {
@@ -25,11 +26,18 @@ function displayHairMaterials(hairMaterials) {
     hairMaterials.forEach(item => {
         const itemDiv = document.createElement("div");
         itemDiv.classList.add("hair-item");
+
+        const image = document.createElement("img")
+        image.src = item.image_url
+        image.alt = item.name
+
+        itemDiv.appendChild(image)
         itemDiv.innerHTML = `
             <h3>${item.name}</h3>
             <p>Price: ${item.price}</p>
             <p>Description: ${item.description}</p>
         `;
+        // container.appendChild(image);
         container.appendChild(itemDiv);
     });
 }
